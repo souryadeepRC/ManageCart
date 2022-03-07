4a8f708b0965aa14f9197b684f790a7f2b4dfa73
Coder Sourya <thecreativeworldforall@gmail.com>
2022-03-07 22:20:47 +0530

    Add the validation of form, success message functionality of item addition to cart

--- js/index.js
+++ js/index.js
@@ -16,30 +16,48 @@ const updateCartItem = (item) => {
         'Type': item.Type
     })
 }
-const isValidDataPresent = (itemName,itemQuantity,itemType) => {
-     
-    return true
+const isValidDataPresent = (itemName,itemQuantity) =>  (itemName.length >0 && itemQuantity.length>0) ?  true : false 
+
+const onItemDetailsChange = () => {
+    console.log($('#ItemName').val().length,$('#ItemQuantity').val().length);
+     if(isValidDataPresent($('#ItemName').val(),$('#ItemQuantity').val())){
+        $('#AddToCart').prop('disabled',false)
+     }else{ 
+        $('#AddToCart').prop('disabled',true)
+     }
 }
+
+const onItemNameChange = () => $('#ItemName').val().length>0
+
 const addItemToCart = () => {
     let itemName  = $('#ItemName').val()
     let itemDescription = $('#ItemDescription').val()
     let itemQuantity = $('#ItemQuantity').val()
     let itemType = $('#ItemType').val()
-    if(isValidDataPresent(itemName,itemQuantity,itemType)){ 
+    if(isValidDataPresent(itemName,itemQuantity)){ 
         cartItemList.push({
             'Name': itemName,
             'Description': itemDescription,
             'Quantity': itemQuantity,
             'Type': itemType
         })
+        clearInputField()
+        $('#SuccessMsg').show().delay(1000).fadeOut()
+        $('#AddToCart').prop('disabled',true)
         return true
     }else{
         return false
     }
 }
+const clearInputField = () => {
+    $('#ItemName').val('')
+    $('#ItemDescription').val('')
+    $('#ItemQuantity').val('') 
+}
+
 $(document).ready(() => {
     console.log('Manage Cart Loaded ...');
-    renderCartItemCount()
+    renderCartItemCount() 
     $('#AddToCart').on('click', () => {
         if(addItemToCart()){
             console.log('Item Added to Cart');
