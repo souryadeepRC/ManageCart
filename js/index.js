0296e9ab89fe1ed3da9709c261069b121bdeae1a
Coder Sourya <thecreativeworldforall@gmail.com>
2022-03-07 21:33:00 +0530

    Adding Header, Footer , add Item Button, Adding the Cart Count Functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+var cartItemList = []
+const renderCartItemCount = () => { 
+    if (cartItemList.length > 0) {
+        $('#CartItemCount').css('display', 'block')
+        $('#CartItemCount').text(cartItemList.length)
+    } else {
+
+        $('#CartItemCount').css('display', 'none')
+    }
+}
+const updateCartItem = (item) => {
+    cartItemList.push({
+        'Name': item.Name,
+        'Description': item.Description,
+        'Quantity': item.Quantity,
+        'Type': item.Type
+    })
+}
+
 $(document).ready(() => {
     console.log('Manage Cart Loaded ...');
+    renderCartItemCount()
+    $('#AddToCart').on('click', () => {
+        cartItemList.push({
+            'Name': 'Cold Drinks',
+            'Description': 'Pepsi can',
+            'Quantity': '2',
+            'Type': 'Pc.'
+        })
+        renderCartItemCount()
+    })
 })
