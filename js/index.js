4773f696c588b1c1da68652e70581c5e2d5b1a8c
Coder Sourya <thecreativeworldforall@gmail.com>
2022-03-08 21:42:40 +0530

    Modifying styles

--- js/index.js
+++ js/index.js
@@ -78,17 +78,17 @@ const ShowCart = () => {
         let content = ''
         cartItemList.forEach((cartItem, index) => {
             content += `
-           <tr><td><div class="cart-data">
-               <div class="cart-item col-12 d-flex justify-content-around">
+           <tr><td><div class="cart-data my-2 mx-0 p-2">
+               <div class="cart-item col-12 d-flex justify-content-around align-items-center m-auto w-100">
                    <p class="print col-2">${index + 1}</p>
                    <div class="col-8">
-                       <h6>${cartItem.Name}</h6>
-                       <p>${cartItem.Description != '' ? cartItem.Description : '&nbsp;'}</p>
+                       <h6 class="m-0">${cartItem.Name}</h6>
+                       <p class="m-0">${cartItem.Description != '' ? cartItem.Description : '&nbsp;'}</p>
                    </div>
-                   <h6 class="col-3 text-center">${cartItem.Quantity} ${cartItem.Type}</h6>
+                   <h6 class="col-3 text-center m-0">${cartItem.Quantity} ${cartItem.Type}</h6>
                </div>
                <div class="d-flex justify-content-end">
-                    <p class="w-15 delete-item not-print m-0 text-center fa fa-trash-o" 
+                    <p class="w-15 delete-item not-print m-0 text-center text-white fa fa-trash-o" 
                         id="ItemDelete${index}" onclick="deleteItem(${index})"> </p>
                 </div>
                </div></td>
@@ -119,6 +119,8 @@ const printReceipt = () => {
 $(document).ready(() => {
 
     cartItemList = loadCartData()
+    populateInputField()
+
     $('#EmptyCartBtn').prop('disabled', !(cartItemList.length > 0))
     console.log('Manage Cart Loaded ...');
     $('#CartDetail').hide()
